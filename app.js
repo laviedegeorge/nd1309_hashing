23ed7869d190ea59d72bb50050928ab737613fd3
laviedegeorge <laviedegeorge1292@gmail.com>
2021-04-26 01:10:50 +0100

    create a hash using crypto-js SHA256 function

--- app.js
+++ app.js
@@ -2,10 +2,11 @@
  * Step 1: Verify 'crypto-js' module is installed for your project
  * After verifying it is installed, move on to Step 2. No code needs to be written here.
  */
-
 /**
  * Step 2: Import from crypto-js module the sha265 library
  */
+//import SHA256 from "crypto-js/sha256";
+var SHA256 = require("crypto-js/sha256");
 
 // Write your code here
 
@@ -15,19 +16,20 @@
 
 const data1 = "Blockchain Rock!";
 const dataObject = {
-	id: 1,
-  	body: "With Object Works too",
-  	time: new Date().getTime().toString().slice(0,-3)
+  id: 1,
+  body: "With Object Works too",
+  time: new Date().getTime().toString().slice(0, -3),
 };
 
 /**
  * Step 3: Add code to the `generate hash function
  * Function that generate the SHA256 Hash
- * @param {*} obj 
+ * @param {*} obj
  */
 
 function generateHash(obj) {
-	// Write your code here
+  // Write your code here
+  return SHA256(JSON.stringify(obj));
 }
 
 console.log(`SHA256 Hash: ${generateHash(data1)}`);
